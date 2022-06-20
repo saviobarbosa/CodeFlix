480a2e34b3f978803b4e074027377fb7d00e1e9e
Sávio Barbosa <saviiobarbosa@gmail.com>
2022-06-20 17:06:03 -0300

    Changes in windows OS

---
+++ tests/FC.CodeFlix.Catalog.UnitTests/Application/CreateCategory/CreateCategoryTestFixture.cs
@@ -0,0 +1,16 @@
+﻿using FC.CodeFlix.Catalog.UnitTests.Common;
+using Xunit;
+
+namespace FC.CodeFlix.Catalog.UnitTests.Application.CreateCategory
+{
+    [CollectionDefinition(nameof(CreateCategoryTestFixture))]
+    public class CreateCategoryTestFixtureCollection : ICollectionFixture<CreateCategoryTestFixture>
+    {
+
+    }
+
+    public class CreateCategoryTestFixture : BaseFixture
+    {
+        
+    }
+}
